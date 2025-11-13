72b63a747ab5080d9e8c32336c2925aa60c025ed
d41dev <d41dev@proton.me>
2025-11-13 18:38:21 +0000

    update: model paths

--- constants.py
+++ constants.py
@@ -1,10 +1,8 @@
 import os 
 
-MODEL_NAME = 'ViT-B-32'
-PRETRAINED = 'laion2b_s34b_b79k'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TEXT_ENCODER_PATH = os.path.join(BASE_DIR, 'clip_text_encoder_quant.onnx')
-IMAGE_ENCODER_PATH = os.path.join(BASE_DIR, 'clip_image_encoder_quant.onnx')
-MINILM_MODEL_PATH = os.path.join(BASE_DIR, 'minilm_sentence_transformer_quant.onnx')
+TEXT_ENCODER_PATH = os.path.join(BASE_DIR, 'models/clip_text_encoder_quant.onnx')
+IMAGE_ENCODER_PATH = os.path.join(BASE_DIR, 'models/clip_image_encoder_quant.onnx')
+MINILM_MODEL_PATH = os.path.join(BASE_DIR, 'models/minilm_sentence_transformer_quant.onnx')
 
 
