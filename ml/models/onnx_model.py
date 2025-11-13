5fd40e37f9c3661f1e61cae699c4203117d78c21
d41dev <d41dev@proton.me>
2025-11-13 01:42:45 +0000

    update: rename dir to ml

---
+++ ml/models/onnx_model.py
@@ -0,0 +1,24 @@
+from ml.models.base_model import BaseModel
+import onnxruntime as ort
+import numpy as np
+
+class OnnxModel(BaseModel):
+    def __init__(self, model_path: str):
+        self.ort_session = None
+        self.model_path = model_path
+
+    def load(self):
+        self.ort_session =  ort.InferenceSession(self.model_path)
+
+
+    def is_load(self) -> bool:
+        return self.ort_session is not None
+    
+    def close(self):
+        self.ort_session = None
+
+    def get_inputs(self):
+        return self.ort_session.get_inputs()
+
+    def run(self, inputs: dict) -> list[np.ndarray]:
+        return self.ort_session.run(None, inputs)
