43aa593e9b1df231c8df8cfb31664de2ea16477d
d41dev <d41dev@proton.me>
2025-11-14 20:51:15 +0000

    update: add method compare_embedding_to_dir

--- smartscan/organiser/analyser.py
+++ smartscan/organiser/analyser.py
@@ -23,44 +23,48 @@ class FileAnalyser():
         self.refresh_prototype_duration = refresh_prototype_duration
 
     def compare_files(self, filepath1: str, filepath2: str):
-        """Compute the cosine similarity between two files' embeddings."""
+        """Compute the cosine similarity between two files"""
         is_image_mode = all(path.lower().endswith(self.valid_img_exts) for path in [filepath1, filepath2])
         is_text_mode = all(path.lower().endswith(self.valid_txt_exts) for path in [filepath1, filepath2])
 
         embedder = self.image_encoder if is_image_mode else (self.text_encoder if is_text_mode else None)
 
-        assert embedder is not None, "Both files must be of the same type e.g both image files or both text files"
+        if embedder is None:
+            raise ValueError("Both files must be of the same type e.g both image files or both text files")
 
         embeddings = embedder.embed_batch([filepath1, filepath2])
         return np.dot(embeddings[0], embeddings[1])
     
-    def compare_file_to_dir(self, filepath: str, dirpath: str):
-        """
-        Compute the cosine similarity between a text file's embedding and a directory's prototype embedding.
-        """
+
+    def compare_embedding_to_dir(self, embedding: np.ndarray, dirpath: str, embedder: EmbeddingProvider):
         prototype_embedding_filepath = os.path.join(dirpath, "prototype_embedding.pkl")
-
-        is_image_mode = all(path.lower().endswith(self.valid_img_exts) for path in [filepath])
-        is_text_mode = all(path.lower().endswith(self.valid_txt_exts) for path in [filepath])
-
-        embedder = self.image_encoder if is_image_mode else (self.text_encoder if is_text_mode else None)
-
-        assert embedder is not None, "Both files must be of the same type e.g both image files or both text files"
-
-    
         if os.path.exists(prototype_embedding_filepath) and not self.is_prototype_stale(prototype_embedding_filepath):
             prototype_embedding = load_embedding(prototype_embedding_filepath)
         else:
             prototype_embedding = self.generate_prototype_for_dir(dirpath, embedder)
             save_embedding(prototype_embedding_filepath, prototype_embedding)
+        return np.dot(embedding, prototype_embedding)
+    
+    
+    def compare_file_to_dir(self, filepath: str, dirpath: str):
+        """
+        Compute the cosine similarity between a file and a directory.
+        """
+        is_image_mode = all(path.lower().endswith(self.valid_img_exts) for path in [filepath])
+        is_text_mode = all(path.lower().endswith(self.valid_txt_exts) for path in [filepath])
+
+        embedder = self.image_encoder if is_image_mode else (self.text_encoder if is_text_mode else None)
+
+        if embedder is None:
+            raise ValueError("Unsupported file type")
 
         file_embedding = embedder.embed(filepath)
-        return np.dot(file_embedding, prototype_embedding)
-    
+        return self.compare_embedding_to_dir(file_embedding, dirpath, embedder)
+
 
     def compare_file_to_dirs(self, filepath: str, dirpaths: List[str]):
             """
-            Compare a file's embedding to prototype embeddings from multiple directories, returning
+            Compute the cosine similarities between a file and multiple directories, returning
             the best matching directory and its similarity score.
             """
             best_similarity = -float("inf")
@@ -71,25 +75,14 @@ class FileAnalyser():
 
             embedder = self.image_encoder if is_image_mode else (self.text_encoder if is_text_mode else None)
 
-            assert embedder is not None, "Both files must be of the same type e.g both image files or both text files"
-
+            if embedder is None:
+                raise ValueError("Unsupported file type")
     
             file_embedding = embedder.embed(filepath)
 
             for dirpath in dirpaths:
-                prototype_embedding_filepath = os.path.join(dirpath, "prototype_embedding.pkl")
                 try:
-                    if os.path.exists(prototype_embedding_filepath) and not self.is_prototype_stale(prototype_embedding_filepath):
-                        prototype_embedding = load_embedding(prototype_embedding_filepath)
-                    else:
-                        prototype_embedding = self.generate_prototype_for_dir(dirpath, embedder)
-                        save_embedding(prototype_embedding_filepath, prototype_embedding)
-                except Exception as e:
-                    print(f"[WARNING] Skipping directory {dirpath} due to error: {e}")
-                    continue
-
-                try:
-                    similarity = np.dot(file_embedding, prototype_embedding)
+                    similarity = self.compare_embedding_to_dir(file_embedding, dirpath, embedder)
                 except Exception as e:
                     print(f"[WARNING] Error comparing embeddings for directory {dirpath}: {e}")
                     continue
@@ -116,6 +109,6 @@ class FileAnalyser():
         prototype_embedding = generate_prototype_embedding(embeddings)
         return prototype_embedding
     
+
     def is_prototype_stale(self, path: str) -> bool:
         return get_days_since_last_modified(path) > self.refresh_prototype_duration
-
