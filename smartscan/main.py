f554ca1bca0b5090b27483ea4260d360f4fa1f2b
d41dev <d41dev@proton.me>
2025-11-16 23:38:18 +0000

    update: Instead of user passing target and destination directories user only needs to pass target directories for scan.

--- smartscan/main.py
+++ smartscan/main.py
@@ -6,7 +6,7 @@ import asyncio
 import chromadb
 import shutil
 
-from smartscan.utils.file import load_dir_list, get_files_from_dirs
+from smartscan.utils.file import load_dir_list, get_files_from_dirs, get_child_dirs
 from smartscan.constants import  DINO_V2_SMALL_MODEL_PATH, MINILM_MODEL_PATH, SCAN_HISTORY_DB, DB_DIR
 from smartscan.organiser.analyser import FileAnalyser
 from smartscan.organiser.scanner import FileScanner
@@ -25,6 +25,36 @@ async def main():
         raise ValueError(f"Image encoder model not found: {DINO_V2_SMALL_MODEL_PATH}")
 
 
+    parser = argparse.ArgumentParser(description="CLI tool for comparing text or image embeddings and scanning directories.")
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    compare_parser = subparsers.add_parser("compare", help="Perform comparisons using different modes.")
+    compare_group = compare_parser.add_mutually_exclusive_group(required=True)
+    compare_group.add_argument("-f", "--file",nargs=2,metavar=("FILEPATH1", "FILEPATH2"),help="Compare two text or image files.")
+    compare_group.add_argument("-d", "--dir",nargs=2,metavar=("FILEPATH", "DIRPATH"),help="Compare a text or image file against a directory.")
+    compare_group.add_argument("-l", "--dirs",nargs=2,metavar=("FILEPATH", "DIRLISTFILE"),help="Compare a text or image file against multiple directories listed in a file.")
+
+    scan_parser = subparsers.add_parser("scan", help="Scan directories and auto organise files into directories.")
+    scan_parser.add_argument("-t", "--threshold",type=float,default=0.4,help="Similarity threshold for the scans. Default is 0.4.")
+    scan_group = scan_parser.add_mutually_exclusive_group(required=True)
+    scan_group.add_argument("dirlist_file", nargs="?", metavar="DIRLISTFILE", help="File listing target directories to scan.")
+    scan_group.add_argument("--dirs",nargs='+',metavar="DIRLIST", help="List of directories to scan.")
+
+    index_parser = subparsers.add_parser("index", help="Index files from selected directories.")
+    index_parser.add_argument("--n-frames",type=int, default=10,help="Number of frames to use when generating video embedding. Default is 10")
+    index_group = index_parser.add_mutually_exclusive_group(required=True)
+    index_group.add_argument("dirlist_file",nargs='?', metavar="DIRLISTFILE", help="File listing target directories to index.")
+    index_group.add_argument("--dirs",nargs='+',metavar="DIRLIST", help="List of directories to index.")
+
+    restore_parser = subparsers.add_parser("restore", help="Restore files that were moved to their original location.")
+    restore_parser.add_argument("--start-date",type=str,default=None,help="The start_date period of when to restore")
+    restore_parser.add_argument("--end-date",type=str,default=None,help="The start_date period of when to restore")
+    restore_group = restore_parser.add_mutually_exclusive_group(required=False)
+    restore_group.add_argument("file",nargs='?',metavar="FILETORESTORE",help="File to restore")
+    restore_group.add_argument("--files",nargs='+',metavar="FILESTORESTORE",help="List of files to restore.")
+
+    args = parser.parse_args()
+
     client = chromadb.PersistentClient(path=DB_DIR)
     text_store = client.get_or_create_collection(
         name="text_collection",
@@ -39,113 +69,20 @@ async def main():
         metadata={"description": "Collection for videos"}
     )
 
-    file_analyser = FileAnalyser(
-        image_encoder=DinoSmallV2ImageEmbedder(DINO_V2_SMALL_MODEL_PATH),
-        text_encoder=MiniLmTextEmbedder(MINILM_MODEL_PATH),
-        similarity_threshold=0.4,
-        image_store=image_store,
-        text_store=text_store,
-        video_store=video_store,
-    )
-
-    file_analyser.image_encoder.init()
-    file_analyser.text_encoder.init()
-
-
-    parser = argparse.ArgumentParser(
-        description="CLI tool for comparing text or image embeddings and scanning directories."
-    )
-    subparsers = parser.add_subparsers(dest='command', required=True)
-
-    # Compare subcommand
-    compare_parser = subparsers.add_parser("compare", help="Perform comparisons using different modes.")
-    compare_group = compare_parser.add_mutually_exclusive_group(required=True)
-    compare_group.add_argument(
-        "-f", "--file",
-        nargs=2,
-        metavar=("FILEPATH1", "FILEPATH2"),
-        help="Compare two text or image files."
-    )
-    compare_group.add_argument(
-        "-d", "--dir",
-        nargs=2,
-        metavar=("FILEPATH", "DIRPATH"),
-        help="Compare a text or image file against a directory."
-    )
-    compare_group.add_argument(
-        "-l", "--dirs",
-        nargs=2,
-        metavar=("FILEPATH", "DIRLISTFILE"),
-        help="Compare a text or image file against multiple directories listed in a file."
-    )
-
-    # Scan subcommand
-    scan_parser = subparsers.add_parser("scan", help="Scan directories and auto organise files into directories.")
-    scan_parser.add_argument(
-        "target_file", metavar="TARGET_FILE", help="File listing target directories."
-    )
-    scan_parser.add_argument(
-        "destination_file", metavar="DESTINATION_FILE", help="File listing destination directories."
-    )
-    scan_parser.add_argument(
-        "-t", "--threshold",
-        type=float,
-        default=0.4,
-        help="Similarity threshold for the scan mode. Default is 0.4."
-    )
-
-    index_parser = subparsers.add_parser("index", help="Index files from selected directories.")
-    index_parser.add_argument(
-        "--n_frames",
-        type=int,
-        default=10,
-        help="The number of frames to use when creating embedding for video. Default is 10"
-    )
-    index_group = index_parser.add_mutually_exclusive_group(required=True)
-    index_group.add_argument(
-        "dirlist_file",
-        nargs='?',
-        metavar="DIRLISTFILE",
-        help="File listing target directories."
-    )
-    index_group.add_argument(
-        "--dirs",
-        nargs='+',
-        metavar="DIRLIST",
-        help="List of directories to index."
-    )
-
-
-    restore_parser = subparsers.add_parser("restore", help="Restore files that were moved to their original location.")
-    restore_parser.add_argument(
-        "--start-date",
-        type=str,
-        default=None,
-        help="The start_date period of when to restore"
-    )
-    restore_parser.add_argument(
-        "--end-date",
-        type=str,
-        default=None,
-        help="The start_date period of when to restore"
-    )
-    restore_group = restore_parser.add_mutually_exclusive_group(required=True)
-    restore_group.add_argument(
-        "file",
-        nargs='?',
-        metavar="FILETORESTORE",
-        help="File to restore"
-    )
-    restore_group.add_argument(
-        "--files",
-        nargs='+',
-        metavar="FILESTORESTORE",
-        help="List of files to restore."
-    )
-
-    args = parser.parse_args()
-
+ 
     if args.command == "compare":
+        file_analyser = FileAnalyser(
+            image_encoder=DinoSmallV2ImageEmbedder(DINO_V2_SMALL_MODEL_PATH),
+            text_encoder=MiniLmTextEmbedder(MINILM_MODEL_PATH),
+            similarity_threshold=0.4,
+            image_store=image_store,
+            text_store=text_store,
+            video_store=video_store,
+        )
+
+        file_analyser.image_encoder.init()
+        file_analyser.text_encoder.init()
+
         if args.file:
             filepath1, filepath2 = args.file
             if not os.path.isfile(filepath1) or not os.path.isfile(filepath2):
@@ -167,19 +104,35 @@ async def main():
             dirpaths = load_dir_list(dirlist_file)
             dirs_similarities = file_analyser.compare_file_to_dirs(filepath, dirpaths)
             print(f"File-to-directories similarity\n--------------------------\n")
-            for (key, value) in sorted(dirs_similarities.items()):
+            for (key, value) in sorted(dirs_similarities.items(), key=lambda x: x[1], reverse=True):
                 print(f"Directory: {key} | Similarity: {value}")
 
     elif args.command == "scan":
+        file_analyser = FileAnalyser(
+            image_encoder=DinoSmallV2ImageEmbedder(DINO_V2_SMALL_MODEL_PATH),
+            text_encoder=MiniLmTextEmbedder(MINILM_MODEL_PATH),
+            similarity_threshold=0.4,
+            image_store=image_store,
+            text_store=text_store,
+            video_store=video_store,
+        )
+
         file_analyser.similarity_threshold = args.threshold
-        if not os.path.isfile(args.target_file) or not os.path.isfile(args.destination_file):
-            raise argparse.ArgumentTypeError(f"Target or destination file not found: {args.target_file}, {args.destination_file}")
-        
-        target_dirs = load_dir_list(args.target_file)
-        destination_dirs = load_dir_list(args.destination_file)
-        file_scanner = FileScanner(analyser=file_analyser, destination_dirs=destination_dirs, db_path=SCAN_HISTORY_DB , listener = FileScannerListener())
+       
+        if args.dirlist_file:
+            if not os.path.isfile(args.dirlist_file):
+                raise argparse.ArgumentTypeError(f"Target file not found: {args.dirlist_file}")
+            target_dirs = load_dir_list(args.target_file)
+        else:
+            target_dirs = args.dirs
+
+        file_analyser.image_encoder.init()
+        file_analyser.text_encoder.init()
+
+        file_scanner = FileScanner(analyser=file_analyser, destination_dirs=get_child_dirs(target_dirs), db_path=SCAN_HISTORY_DB , listener = FileScannerListener())
         allowed_exts = file_analyser.valid_img_exts + file_analyser.valid_txt_exts + file_analyser.valid_vid_exts
-        result = await file_scanner.run(get_files_from_dirs(target_dirs, allowed_exts=allowed_exts))
+        files = get_files_from_dirs(target_dirs, allowed_exts=allowed_exts)
+        result = await file_scanner.run(files)
         print(f"Scan results - files moved: {result.total_processed} | time elpased: {result.time_elapsed:.2f}s")
     
     elif args.command == "index":
@@ -256,9 +209,6 @@ async def main():
             for f in restore_failed:
                 print(f"Failed to restore: {f}")
         
-
-
-
 
 if __name__ == "__main__":
     asyncio.run(main())
