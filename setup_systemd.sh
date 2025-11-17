02d06915b2a2197cf4eb5b30df3dcb3234731746
d41dev <d41dev@proton.me>
2025-11-17 10:57:24 +0000

    update: systemd setup script

--- setup_systemd.sh
+++ setup_systemd.sh
@@ -2,26 +2,15 @@
 
 BASE_DIR="$HOME/.smartscan"
 
-if [ ! -f "$BASE_DIR/smartscan.conf" ]; then
-  echo "Error: $BASE_DIR/smartscan.conf not found. Please make sure the configuration file is present."
+if [ ! -f "$BASE_DIR/smartscan.json" ]; then
+  echo "Error: $BASE_DIR/smartscan.json not found. Please make sure the configuration file is present."
   exit 1
 fi
 
-if ! grep -q "TARGET_FILE" "$BASE_DIR/smartscan.conf" || ! grep -q "DESTINATION_FILE" "$BASE_DIR/smartscan.conf"; then
-  echo "Error: Missing required values (TARGET_FILE or DESTINATION_FILE) in $BASE_DIR/smartscan.conf"
-  exit 1
-fi
+TARGET_DIRS=$(jq ".target_dirs[]" "$BASE_DIR/smartscan.json")
 
-TARGET_FILE=$(grep -oP '^TARGET_FILE=\K.+' "$BASE_DIR/smartscan.conf")
-DESTINATION_FILE=$(grep -oP '^DESTINATION_FILE=\K.+' "$BASE_DIR/smartscan.conf")
-
-if [ ! -f "$TARGET_FILE" ]; then
-  echo "Error: TARGET_FILE ($TARGET_FILE) is not a valid file path."
-  exit 1
-fi
-
-if [ ! -f "$DESTINATION_FILE" ]; then
-  echo "Error: DESTINATION_FILE ($DESTINATION_FILE) is not a valid file path."
+if [ ${#TARGET_DIRS[@]} -lt 1 ]; then
+  echo "Error: Target directories (target_dirs) is empty."
   exit 1
 fi
 
