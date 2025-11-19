7d469525b7699823586b7df78b46a1eb46d5ec41
d41dev <d41dev@proton.me>
2025-11-19 17:33:55 +0000

    update: constnats

--- smartscan/constants.py
+++ smartscan/constants.py
@@ -1,25 +1,4 @@
-from typing import Literal
-
 class SupportedFileTypes:
     IMAGE = ('.png', '.jpg', '.jpeg', '.bmp', '.webp')
     TEXT = ('.txt', '.md', '.rst', '.html', '.json')
     VIDEO = ('.mp4', '.mkv', '.webm')
-
-
-ModelName = Literal[
-    "clip-vit-b-32-image",
-    "clip-vit-b-32-text",
-    "dinov2-small",
-    "inception-resnet-v1",
-    "all-minilm-l6-v2",
-]
-
-EncoderType = Literal["image_encoder", "text_encoder", "face_encoder"]
-
-MODEL_REGISTRY: dict[ModelName, EncoderType] = {
-    "clip-vit-b-32-image": "image_encoder",
-    "clip-vit-b-32-text": "text_encoder",
-    "dinov2-small": "image_encoder",
-    "inception-resnet-v1": "face_encoder",
-    "all-minilm-l6-v2": "text_encoder",
-}
