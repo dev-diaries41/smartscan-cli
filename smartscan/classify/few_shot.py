4494a44e7ca714a690d560317a7c9bf4c148f008
d41dev <d41dev@proton.me>
2026-07-03 18:03:08 +0100

    fix: use StoredEmbedding and remove usage of sim_factor

--- smartscan/classify/few_shot.py
+++ smartscan/classify/few_shot.py
@@ -2,27 +2,25 @@ import numpy as np
 from smartscan.processor import BatchProcessor, ProcessorListener
 from smartscan.classify.types import  ClassificationResult
 from smartscan.cluster.types import  Cluster
-from smartscan.embeds.types import ItemEmbedding
+from smartscan.embeds.types import StoredEmbedding
 
-class FewShotClassifier(BatchProcessor[ItemEmbedding, ClassificationResult]):
+class FewShotClassifier(BatchProcessor[StoredEmbedding, ClassificationResult]):
     def __init__(self, 
                 labelled_clusters: list[Cluster],
-                listener: ProcessorListener[ItemEmbedding, ClassificationResult],
-                sim_factor: float = 1.0,
+                listener: ProcessorListener[StoredEmbedding, ClassificationResult],
                 **kwargs
                 ):
         super().__init__(listener=listener, **kwargs)
         self.labelled_clusters = labelled_clusters
-        self.sim_factor = sim_factor
 
-    def on_process(self, item: ItemEmbedding) -> ClassificationResult:
-        return few_shot_classify(item, self.labelled_clusters, self.sim_factor)
+    def on_process(self, item: StoredEmbedding) -> ClassificationResult:
+        return few_shot_classify(item, self.labelled_clusters)
     
     async def on_batch_complete(self, batch):
         await self.listener.on_batch_complete(batch)
 
 
-def few_shot_classify(item: ItemEmbedding, labelled_clusters: list[Cluster], sim_factor: float = 1.0) -> ClassificationResult:
+def few_shot_classify(item: StoredEmbedding, labelled_clusters: list[Cluster]) -> ClassificationResult:
         label = None
         best_sim = 0.0
         
@@ -31,7 +29,8 @@ def few_shot_classify(item: ItemEmbedding, labelled_clusters: list[Cluster], sim
                 similarity = np.dot(item.embedding, cluster.embedding)
             except Exception as e:
                 continue
-            if similarity > best_sim and similarity >= sim_factor * cluster.metadata.mean_similarity:
+            threshold = cluster.metadata.mean_similarity - cluster.metadata.std_similarity
+            if similarity > best_sim and similarity >= threshold:
                 label = cluster.label
                 best_sim = similarity
         return ClassificationResult(item_id=item.item_id, label=label, similarity=float(best_sim))
